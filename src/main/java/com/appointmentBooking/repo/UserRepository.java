595e0e71dad9fb5ee95b47149cc410aa7c1df5d0
Kshitij-Bajaj <ankitchopra6396@gmail.com>
2023-05-23 17:01:18 +0530

    ok

--- src/main/java/com/appointmentBooking/repo/UserRepository.java
+++ src/main/java/com/appointmentBooking/repo/UserRepository.java
@@ -1,5 +1,6 @@
 package com.appointmentBooking.repo;
 import org.springframework.data.repository.CrudRepository;
+
 import com.appointmentBooking.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
