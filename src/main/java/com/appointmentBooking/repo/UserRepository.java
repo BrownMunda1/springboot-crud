a9797aa95c6d9224c9d718db4490f4ebe6f5557d
Kshitij-Bajaj <kshitij.bajaj@mykaarma.com>
2023-05-23 16:56:55 +0530

    initial commit

--- src/main/java/com/appointmentBooking/repo/UserRepository.java
+++ src/main/java/com/appointmentBooking/repo/UserRepository.java
@@ -1,6 +1,5 @@
 package com.appointmentBooking.repo;
 import org.springframework.data.repository.CrudRepository;
-
 import com.appointmentBooking.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
